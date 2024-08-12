6445f5164d09f175f6915e6fcb0784975892b59f
chenaoh <Fabionahuelgarcia@gmail.com>
2024-08-11 21:48:31 -0300

    node.js

---
+++ data/movies.js
@@ -0,0 +1,74 @@
+const movies = [
+    {
+      id: 1,
+      title: "Inception",
+      director: "Christopher Nolan",
+      release: 2010,
+      rating: 8.8
+    },
+    {
+      id: 2,
+      title: "The Shawshank Redemption",
+      director: "Frank Darabont",
+      release: 1994,
+      rating: 9.3
+    },
+    {
+      id: 3,
+      title: "The Godfather",
+      director: "Francis Ford Coppola",
+      release: 1972,
+      rating: 9.2
+    },
+    {
+      id: 4,
+      title: "The Dark Knight",
+      director: "Christopher Nolan",
+      release: 2008,
+      rating: 9.0
+    },
+    {
+      id: 5,
+      title: "Pulp Fiction",
+      director: "Quentin Tarantino",
+      release: 1994,
+      rating: 8.9
+    },
+    {
+      id: 6,
+      title: "The Lord of the Rings: The Return of the King",
+      director: "Peter Jackson",
+      release: 2003,
+      rating: 8.9
+    },
+    {
+      id: 7,
+      title: "Fight Club",
+      director: "David Fincher",
+      release: 1999,
+      rating: 8.8
+    },
+    {
+      id: 8,
+      title: "Forrest Gump",
+      director: "Robert Zemeckis",
+      release: 1994,
+      rating: 8.8
+    },
+    {
+      id: 9,
+      title: "The Matrix",
+      director: "Lana Wachowski, Lilly Wachowski",
+      release: 1999,
+      rating: 8.7
+    },
+    {
+      id: 10,
+      title: "Star Wars: Episode V - The Empire Strikes Back",
+      director: "Irvin Kershner",
+      release: 1980,
+      rating: 8.7
+    }
+  ];
+
+  module.exports = movies;
